802425eee6ba209554bd4831a285b1dcf49d86ff
Araba Taylor <araba.taylor20@gmail.com>
2017-11-12 18:05:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,7 +17,7 @@ class Song
     split_filename = filename.split(" - ")
     @artist = split_filename[0]
     @name = split_filename[1]
-    Song.new(filename)
+    Song.new(split_filename)
     #filename = filename.split(" - ")
     #Song.new(filename)
   end
