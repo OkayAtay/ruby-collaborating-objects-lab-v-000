d64df37001cc8f98fc029c4400401ea0920d25a5
Araba Taylor <araba.taylor20@gmail.com>
2017-11-12 18:25:25 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,8 @@ class Song
     @name=name
   end
 
-  def artist
-    self.artist = @artist
+  def artist(name)
+    self.artist = Artist.find_or_create_by_name()
   end
 
   def self.new_by_filename(filename)
