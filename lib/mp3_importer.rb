ff0478d93e79394b43e2fce869e17853cc8ecbc8
Araba Taylor <araba.taylor20@gmail.com>
2017-11-12 17:55:51 +0000

    automatically backed up by learn

---
+++ lib/mp3_importer.rb
@@ -0,0 +1,22 @@
+require 'pry'
+
+class MP3Importer
+
+  attr_accessor :path, :files
+
+  def initialize(path)
+    @path = path
+  end
+
+
+  def files
+    binding.pry
+    @files = []
+    @files ||= Dir[@path+"/*"].each{|file| file.split("./spec/fixtures/mp3s/")[1]}
+
+  end
+
+  def import
+    files.each{|filename| Song.new_by_filename(filename)}
+  end
+end
---
+++ lib/song.rb
@@ -0,0 +1,24 @@
+require 'pry'
+
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name=name
+  end
+
+  def artist
+    self.artist = @artist
+  end
+
+  def self.new_by_filename(filename)
+    #binding.pry
+    split_filename = filename.split(" - ")
+    @artist = split_filename[0]
+    @name = split_filename[1]
+    Song.new(@name)
+    #filename = filename.split(" - ")
+    #Song.new(filename)
+  end
+end
